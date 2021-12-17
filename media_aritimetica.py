e49af47488fa4c422dd5859b42c084135a238770
eumesmolnx <eumesmolnx@yahoo.com>
2021-12-17 00:01:21 -0300

    Arithmetic_averege.py

---
+++ media_aritimetica.py
@@ -0,0 +1,7 @@
+#media aritimetica de quatro provas
+nota1 = float(input('Digite a nota da Prova 01: '))
+nota2 = float(input('Digite a nota da Prova 02: '))
+nota3 = float(input('Digite a nota da Prova 03: '))
+nota4 = float(input('Digite a nota da Prova 04: '))
+media = (nota1+nota2+nota3+nota4)/4
+print('A média aritimética é', media)
